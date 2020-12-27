1dff09c80b98fa0dca94471c9f8a5969cf8ef5e0
Ayoub Boulahtar <ayoub.boulahtar@gmail.com>
2020-12-27 17:12:25 +0100

    Established the sign in process

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,13 +6,23 @@ const handleErrors = (err) => {
     console.log(err.message, err.code);
     let errors = { firstName:'', lastName: '', email: '', password: '' };
 
-    //duplicate error code
+    // Incorrect email
+    if (err.message === 'Incorrect email') {
+        errors.email = 'This email is not registered';
+    }
+
+    // Incorrect password
+    if (err.message === 'Incorrect password') {
+        errors.password = 'The password is incorrect';
+    }
+
+    // Duplicate error code
     if (err.code === 11000) {
         errors.email = 'This email is already registered';
         return errors;
     }
 
-    //validation errors
+    // Validation errors
     if (err.message.includes('user validation failed')) {
         Object.values(err.errors).forEach(({properties}) => {
             errors[properties.path] = properties.message;
@@ -53,5 +63,16 @@ module.exports.signup_post = async (req, res) => {
 }
 
 module.exports.signin_post = async (req, res) => {
-    res.send('Logged in successfully');
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.login(email,password)
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({user: user._id});
+    }
+    catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
 }
